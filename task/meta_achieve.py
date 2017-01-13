8f724b6d7c9f243be8708c4910049a6da600fb66
mzfenng <mzfenng@gmail.com>
2017-01-14 01:10:37 +0800

    get okcoin data from ws

--- task/meta_achieve.py
+++ task/meta_achieve.py
@@ -1,14 +1,48 @@
-import sys
+import sys, json
+
 sys.path.append('..')
 import env
 from time import sleep
-
 from datetime import datetime
 from task.request import ConcurrentRequest
-
 from apis import ok_api, huobi_api
 from charts.models import OKCoinSpot, HuobiSpot, Config
 from task.schedule import schedule
+from apis.ws import WS
+
+
+def get_okcoin_from_ws():
+    def on_okcoin_open(self, ws):
+        ws.send("{'event':'addChannel','channel':'ok_btccny_ticker'}")
+        print "## open ##"
+
+    def _on_okcoin_message(self, ws, message):
+        message = message[0]
+        if 'data' in message:
+            ok_datas = {
+                'date_stamp': float(message['data']['timestamp']),
+                'date_time': datetime.fromtimestamp(float(message['data']['timestamp'])),
+                'buy': message['data']['buy'],
+                'high': message['data']['high'],
+                'last': message['data']['last'],
+                'low': message['data']['low'],
+                'sell': message['data']['sell'],
+                'vol': message['data']['vol'],
+                'symbol': 'btccny'
+            }
+        okcoin = OKCoinSpot(**ok_datas)
+        okcoin.save()
+        #TODO
+        print 'message:', message
+        data = json.loads(message)
+        self.on_message(ws, data)
+
+    ws = WS('wss://real.okcoin.cn:10440/websocket/okcoinapi')
+    ws.on_open = on_okcoin_open
+    ws._on_message = _on_okcoin_message()
+    ws.asyc_start()
+
+
 
 
 def get_market():
@@ -19,7 +53,6 @@ def get_market():
 
     ok_res, huobi_res = creq.start()
     if ok_res and huobi_res:
-
         ok_datas = {
             'date_stamp': float(ok_res['date']),
             'date_time': datetime.fromtimestamp(float(ok_res['date'])),
